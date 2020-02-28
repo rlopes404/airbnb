08ade3ce39037f951fa70697cd30c8c1e3cf5ce5
rlopes404 <ramonp88@gmail.com>
2020-02-28 20:42:52 -0300

    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created on Fri Feb 28 06:28:50 2020
 
 @author: ramon
 """
-#TODO: latitude, longitude, 
+#TODO: latitude, longitude,
 
 import pandas as pd
 import numpy as np
@@ -35,7 +35,7 @@ from sklearn.ensemble import StackingRegressor
 #Previsão do preço da estadia (feature ‘price’)
 #Classificação do room type (feature ‘room_type’)
 
-#Faça uma análise exploratória para avaliar a consistência dos dados 
+#Faça uma análise exploratória para avaliar a consistência dos dados
 #e identificar possíveis variáveis que impactam sua variável resposta
 
 ## load data
@@ -46,11 +46,11 @@ y_name = 'price'
 ##exploring the dataset
 data.head(n=10)
 
-data.shape 
+data.shape
 #(33715, 16)
 
 data.info()
-#name, host_name, neighbourhood, room_type, last_review are not numeric 
+#name, host_name, neighbourhood, room_type, last_review are not numeric
 #missing data: neighbourhood_group, last_review, reviews_per_month
 #to keep: neibhourhood, room_type, last_review
 #discard: id, name, host_id, host_name
@@ -185,7 +185,7 @@ results.sort()
 best_d = results[0][1]
 
 poly = PolynomialFeatures(degree=best_d, interaction_only=True, include_bias = True)
-X_train_poly = poly.fit_transform(X_train) 
+X_train_poly = poly.fit_transform(X_train)
 X_test_poly = poly.fit_transform(X_test)
 
 lr_poly =  LinearRegression().fit(X_train_poly, Y_train)
@@ -206,17 +206,17 @@ print('Improved LR RMSE: %.2f' %(np.sqrt(MSE)))
 
 ### XGBoost
 grid_params = {
-            'learning_rate': [0.01, 0.05, 0.1],
-            'n_estimators': [100, 500, 1000],
-            'max_depth': [2, 6, 10],
-            'subsample': [0.3, 0.7, 1.0],
-            'colsample_bytree': [0.3, 0.7, 1.0],
-            'min_child_weight': [1, 10, 100],
+            'learning_rate': [0.001, 0.01],
+            'n_estimators': [100, 500],
+            'max_depth': [5, 10],
+            'subsample': [0.5, 1.0],
+            'colsample_bytree': [0.5, 1.0],
+            'min_child_weight': [1, 10],
             'early_stopping_rounds': [10],
-            'gamma': [0, 1, 5]
+            'gamma': [0, 1]
             }
       
-xgb = GridSearchCV(estimator=XGBRegressor(seed=42), param_grid=grid_params)
+xgb = GridSearchCV(estimator=XGBRegressor(seed=42), cv=2, param_grid=grid_params, verbose=1)
 xgb.fit(X_train, Y_train)
 
 
@@ -227,11 +227,36 @@ Y_pred = xgb.predict(X_test)
 RMSE_xgb = np.sqrt(metrics.mean_squared_error(Y_test, Y_pred))
 print('XGB RMSE: %.f' %(RMSE_xgb))
 
+### stacking manual
+
+
+X_valid_poly = poly.fit_transform(X_valid)
+lrp_y_pred = lr_poly.predict(X_valid_poly).reshape(-1,1)
+knn_y_pred = knn.predict(X_valid).reshape(-1,1)
+xgb_y_pred = xgb.predict(X_valid).reshape(-1,1)
+
+_X_train = np.concatenate([xgb_y_pred, knn_y_pred, lrp_y_pred], axis=1)
+
+_lr = LinearRegression().fit(_X_train, Y_valid)
+
+X_test_poly = poly.fit_transform(X_test)
+lrp_y_pred = lr_poly.predict(X_test_poly).reshape(-1,1)
+knn_y_pred = knn.predict(X_test).reshape(-1,1)
+xgb_y_pred = xgb.predict(X_test).reshape(-1,1)
+_X_test = np.concatenate([xgb_y_pred, knn_y_pred, lrp_y_pred], axis=1)
+
+
+Y_pred = _lr.predict(_X_test)
+MSE = metrics.mean_squared_error(Y_test, Y_pred)
+print('Improved LR RMSE: %.2f' %(np.sqrt(MSE)))
 
 ### Stacking
 estimators = [('knn', knn), ('lr_poly', lr_poly), ('xgb', xgb)]
-reg = StackingRegressor(estimators=estimators, random_state=0)
-reg.fit(X_train, Y_train)
+reg = StackingRegressor(estimators=estimators)
+reg.fit(X_test, Y_test)
+Y_pred = reg.predict(X_test)
+MSE = metrics.mean_squared_error(Y_test, Y_pred)
+print('Stacking RMSE: %.2f' %(np.sqrt(MSE)))
 
 ### XGBoost Poly
 poly = PolynomialFeatures(degree=2, interaction_only=True, include_bias = True)
@@ -260,3 +285,11 @@ grid.fit(X_train_poly, Y_train)
 Y_pred = grid.predict(X_test_poly)
 RMSE_xgb = np.sqrt(metrics.mean_squared_error(Y_test, Y_pred))
 print('RMSE_xgb: %.f' %(RMSE_xgb))
+
+
+
+#http://rasbt.github.io/mlxtend/user_guide/regressor/StackingRegressor/
+#https://stackoverflow.com/questions/51194303/how-to-run-a-python-script-in-a-py-file-from-a-google-colab-notebook
+#https://machinelearningmastery.com/stacking-ensemble-for-deep-learning-neural-networks/
+
+#https://towardsdatascience.com/feature-engineering-for-machine-learning-3a5e293a5114
